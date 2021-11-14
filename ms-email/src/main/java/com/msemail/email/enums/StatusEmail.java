056097f30792bd93aa20c6f2726ad96d8b7d7035
fhilips <fhilips.souza@gmail.com>
2021-11-14 17:30:25 -0300

    ms-email

---
+++ ms-email/src/main/java/com/msemail/email/enums/StatusEmail.java
@@ -0,0 +1,7 @@
+package com.msemail.email.enums;
+
+public enum StatusEmail {
+    PROCESSING,
+    SENT,
+    ERROR;
+}
---
+++ ms-email/src/main/java/com/msemail/email/repositories/EmailRepository.java
@@ -0,0 +1,11 @@
+package com.msemail.email.repositories;
+
+import java.util.UUID;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.msemail.email.models.EmailModel;;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
