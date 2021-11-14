e73046eb67b4090fb28496b199473e4c311c225c
fhilips <fhilips.souza@gmail.com>
2021-11-14 18:19:11 -0300

    Integranting with ampq

---
+++ ms-email/src/main/java/com/msemail/email/consumers/EmailConsumer.java
@@ -0,0 +1,26 @@
+package com.msemail.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.msemail.email.dtos.EmailDto;
+import com.msemail.email.models.EmailModel;
+import com.msemail.email.services.EmailService;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+    }
+}
